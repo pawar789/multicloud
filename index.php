ba14c67bcf802860587c2ff83b21efb97093c3c6
pawar789 <66726349+pawar789@users.noreply.github.com>
2020-07-18 15:59:38 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,28 +1,10 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title>Welcome on lw</title>
-	<style type="text/css">
-		body{
-			
-			 background-image: url('sbitm.png');
-
-     color:none;
-    background:blue;
-    font-family: 'Roboto', sans-serif;
-    background-repeat: no-repeat;
-    background-attachment: fixed;
-		}
-		h1{
-			color: white;
-			font-family: sans-serif;
-			text-decoration: underline;
-		}
-	</style>
-</head>
-<body>
+<body bgcolor='#elelel'>
 	
-<center><h1>Thank you vimal sir</h1></center>
-<img src="banner-jpg_710x400xt.jpg">
-</body>
-</html>
+<pre>
+<?php
+print "hii LW/n'";
+print 'ifconfig';
+?>
+
+</pre>
+
