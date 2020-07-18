55239203891ff398da51f197c2b1e027f4cd27c8
pawar789 <66726349+pawar789@users.noreply.github.com>
2020-07-18 16:15:31 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,10 +1,21 @@
-<body bgcolor='#00FFFF'>
-	
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Welcome to the Cloud </title>
+  </head>
+<body>
+
+<h1>"Things do not happen. Things are made to happen !" </h1>
+
 <pre>
 <?php
-print "hii LW/n'";
-print 'ifconfig';
-?>
 
+$file = file_get_contents('url.txt');
+
+echo '<img src="'.$file.'"  width="500" height="600">';
+?>
 </pre>
 
+
+</body>
+</html>
